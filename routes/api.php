7110c627177c3997ad11ccd380a6a4f159560988
Bashir Akle <bashir.akle@gmail.com>
2022-01-17 13:23:14 -0500

    refactor: Add scenario as a route parameter

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('purchases', [PurchaseController::class, 'show'])->name('purchases.show');
+Route::get('purchases/{scenario}', [PurchaseController::class, 'show'])->name('purchases.show');
